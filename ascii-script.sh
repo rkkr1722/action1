f25d2e3b6428a2f11ae3d486b544e2d571675d61
rkkr1722 <rkkr172@zohomail.in>
2025-12-11 07:20:34 +0530

    fix-cowsay

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt get install cowsay
-cowsay -f dragon "Run for Cover, I'm a dragon...RAWR" >> dragon.txt
 ls -la
+sleep 5
+cowsay -f dragon "Run for Cover, I'm a dragon...RAWR" >> dragon.txt
 cat dragon.txt
 grep -i "dragon" dragon.txt
